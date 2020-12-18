7024018217b6c46940694146c9ce2ef0f7b564b8
Dragon <dragon@MacBook-Air-de-Alan.local>
2020-12-18 16:27:29 -0600

    v1.1

--- src/Counter.js
+++ src/Counter.js
@@ -3,6 +3,17 @@ import React, { useState } from "react";
 export default function Counter({ start }) {
     const [count, setCount] = useState(start || 0);
 
+    // ANTI-PATRONES
+    // 1. Evitar colocar lógica de negocio, no colocar funcionalidad/reglas de negocio
+    // 2. Evitar colocar múlples componentes por archivo
+    // 3. Evitar mezclar hooks, fetches, components, functions -> separar cada uno en un archivo
+    // 4. Si agrupar en carpetas contextos, reductores
+    // 5. Evitar mezclar llaves (key={...}) con índices de las listas (.map((_, index)))
+    // 6. Evitar llaves aleatorias
+    // 7. Evitar no colocar llaves en componentes dinámicos
+    // 8. Evitar pasar estados entre componentes -> usar contextos o reductores ~[value, setValue]~
+    // 9. No crear hooks que no hagan una tarea específica
+
     return (
         <div className="d-flex flex-column align-items-center border p-4">
             <span>{count}</span>
